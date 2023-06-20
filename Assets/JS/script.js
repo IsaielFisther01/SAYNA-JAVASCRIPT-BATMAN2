9502c7acc7eb0dcd7f4af572685ad62480be2363
ISAIEL FISTHER BEHAJARIVOLA <priaas823@gmail.com>
2023-06-20 15:06:06 +0200

    ajouter mes derniers commit

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,45 +1,39 @@
-/**
- * || SCROLL PROGRESSIVE
-  */
-// Récupération des boutons Batman
+
 const batmanUpButton = document.getElementById("batman-up-button");
 const batmanDownButton = document.getElementById("batman-down-button");
 
-// Gestionnaire d'événement de clic pour le bouton Batman vers le haut
+
 batmanUpButton.addEventListener("click", () => {
-  // Récupération de la section précédente
+  
   const scrollOptions = {
     top: 0,
     behavior: "smooth" 
   };
 
-  // Ajustez la vitesse de défilement en modifiant la durée en millisecondes
+  
   scrollOptions.duration = 50000;
 
   window.scrollTo(scrollOptions);
 });
 
-// Gestionnaire d'événement de clic pour le bouton Batman vers le bas
+
 batmanDownButton.addEventListener("click", () => {
-  // Récupération de la section suivante
+  
   const scrollOptions = {
     top: document.body.scrollHeight,
-    behavior: "smooth" // ou "auto" pour un défilement instantané
+    behavior: "smooth" 
   };
 
-  // Ajustez la vitesse de défilement en modifiant la durée en millisecondes
+  
   scrollOptions.duration = 50000;
 
   window.scrollTo(scrollOptions);
 });
 
-/**
- *  || APPARITION PROGRESSIVE WITH ANIMATION FADE IN 
- */
-// Récupérez tous les éléments avec la classe "fade-slide"
+
 const fadeSlideElements = document.querySelectorAll('.fade-in');
 
-// Fonction pour vérifier si un élément est visible à l'écran
+
 function isElementVisible(element) {
   const rect = element.getBoundingClientRect();
   return (
@@ -48,7 +42,7 @@ function isElementVisible(element) {
   );
 }
 
-// Fonction pour gérer le défilement et l'apparition progressive des éléments avec un effet de slide
+
 function handleScrollFadeSlide() {
   fadeSlideElements.forEach((element) => {
     if (isElementVisible(element)) {
@@ -59,17 +53,16 @@ function handleScrollFadeSlide() {
   });
 }
 
-// Ajoutez un gestionnaire d'événement de défilement
+
 window.addEventListener('scroll', handleScrollFadeSlide);
 
-// Appelez la fonction handleScrollFadeSlide 
+
 handleScrollFadeSlide();
 
-// APPARITION PROGRESSIVE WITH ZOOM ANIMATION
-// Récupérez toutes les images avec la classe "fade-zoom"
+
 const fadeZoomImages = document.querySelectorAll('.fade-zoom');
 
-// Fonction pour vérifier si une image est visible à l'écran
+
 function isImageVisible(image) {
   const rect = image.getBoundingClientRect();
   return (
@@ -78,7 +71,7 @@ function isImageVisible(image) {
   );
 }
 
-// Fonction pour gérer le défilement et l'apparition progressive
+
 function handleScrollFadeZoom() {
   fadeZoomImages.forEach((image) => {
     if (isImageVisible(image)) {
@@ -89,63 +82,56 @@ function handleScrollFadeZoom() {
   });
 }
 
-// Ajoutez un gestionnaire d'événement de défilement 
+
 window.addEventListener('scroll', handleScrollFadeZoom);
 
-// Appelez la fonction handleScrollFadeZoom 
 handleScrollFadeZoom();
 
-/** 
- *  || ANIMATION CANVAS
- */
-// Get the canvas context
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-// Set the canvas position to fill the entire window
+
 canvas.style.top = 0;
 canvas.style.left = 0;
 canvas.style.width = "100vw";
 canvas.style.height = "100vh";
 
-// Create an object to store mouse coordinates
+
 const mouse = {
     x: 0,
     y: 0,
 };
 
-// Load the image
+
 const image = new Image();
-image.src = "assets/images/logos/pointer.png";
+image.src = "Assets/Image/Logos/pointer.png";
 image.style.width = '1vw';
 image.style.height = '1vw';
-// Define the desired size of the image
+
 const imageSize = {
     width: 20,
     height: 20,
 };
 
-// Listen for mouse movement event
+
 document.addEventListener("mousemove", function (event) {
     mouse.x = event.clientX;
     mouse.y = event.clientY;
 });
 
-// Draw the image at the mouse coordinates
+
 function drawImage() {
-    // Clear the canvas
+    
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Draw the image at the mouse coordinates
     const imageX = mouse.x - imageSize.width / 2;
     const imageY = mouse.y - imageSize.height / 2;
     ctx.drawImage(image, imageX, imageY);
 }
 
-// Wait for the image to load before drawing it
 image.onload = function () {
     drawImage();
 };
 
-// Redraw the image on mouse movement
+
 document.addEventListener("mousemove", drawImage);
